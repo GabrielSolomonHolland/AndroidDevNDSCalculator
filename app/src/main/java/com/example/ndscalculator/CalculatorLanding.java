53cee420f10a2211d86dab850a16f4a98481f12f
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-02-28 13:34:03 -0600

    basic landing page layout + comments

--- app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
+++ app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
@@ -10,4 +10,13 @@ public class CalculatorLanding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    /*
+    hear me out
+
+    we are going to need to use intents like there is no tomorrow for this page
+
+    switch case of buttons? I feel like a switch case for all of the things would be really helpful and efficient
+    it seems like the code on this page will be a complete nightmare but I think we've got it
+     */
 }
