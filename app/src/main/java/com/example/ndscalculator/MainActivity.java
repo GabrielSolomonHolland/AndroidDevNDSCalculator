a8189e36b415bc167e182a3486d1a581d9804373
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-02-28 15:18:47 -0600

    add switch for navigating from home screen

--- app/src/main/java/com/example/ndscalculator/MainActivity.java
+++ app/src/main/java/com/example/ndscalculator/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ndscalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void nav(View v)
+    {
+        Button calc = findViewById(R.id.calcBTN);
+        Button equations = findViewById(R.id.seeEquationsBTN);
+        Button tables = findViewById(R.id.seeTablesBTN);
+
+        Intent toCalc = new Intent(this, CalculatorLanding.class);
+        Intent toEquations = new Intent(this, SeeEquations.class);
+        Intent toTables = new Intent(this, SeeTables.class);
+        switch (v.getId())
+        {
+            case (R.id.calcBTN):
+                startActivity(toCalc);
+            case (R.id.seeEquationsBTN):
+                startActivity(toEquations);
+            case (R.id.seeTablesBTN):
+                startActivity(toTables);
+        }
     }
 }
