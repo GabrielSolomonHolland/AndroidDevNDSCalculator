597194cd0dbe4de445b51a893c454936b58f60e7
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-02-28 13:01:35 -0600

    add base (empty) documents for pages

---
+++ app/src/main/java/com/example/ndscalculator/SeeEquations.java
@@ -0,0 +1,13 @@
+package com.example.ndscalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeeEquations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
