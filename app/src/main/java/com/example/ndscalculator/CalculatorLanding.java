d748a42553e4b6c6e91df0871f5b2bea72814196
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-03-01 09:54:31 -0600

    added skeleton case values

--- app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
+++ app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
@@ -27,8 +27,38 @@ public class CalculatorLanding extends AppCompatActivity {
     {
         switch(v.getId())
         {
-            case (R.id.case1):
+            case (R.id.case1BTN):
                 Intent case1 = new Intent(Case1.class);
+                startActivity(case1);
+            case (R.id.case2BTN):
+                Intent case2 = new Intent(Case2.class);
+                startActivity(case2);
+            case (R.id.case3BTN):
+                Intent case3 = new Intent(Case3.class);
+                startActivity(case3);
+            case (R.id.case4BTN):
+                Intent case4 = new Intent(Case4.class);
+                startActivity(case4);
+            case (R.id.case5BTN):
+                Intent case5 = new Intent(Case5.class);
+                startActivity(case5);
+                /*
+            case (R.id.case6BTN):
+                Intent case6 = new Intent(Case6.class);
+                startActivity(case6);
+            case (R.id.case7BTN):
+                Intent case7 = new Intent(Case7.class);
+                startActivity(case7);
+            case (R.id.case8BTN):
+                Intent case8 = new Intent(Case8.class);
+                startActivity(case8);
+            case (R.id.case9BTN):
+                Intent case9 = new Intent(Case9.class);
+                startActivity(case9);
+            case (R.id.case10BTN):
+                Intent case10 = new Intent(Case10.class);
+                startActivity(case10);
+                 */
         }
     }
 }
---
+++ app/src/main/java/com/example/ndscalculator/Case4.java
@@ -0,0 +1,14 @@
+package com.example.ndscalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Case4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case4);
+    }
+}
