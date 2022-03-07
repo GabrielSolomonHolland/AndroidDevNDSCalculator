c1b186ccd946084b4ddb50f974169ca791817795
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-03-07 13:58:24 -0600

    implement functional spinners

--- app/src/main/java/com/example/ndscalculator/Case1.java
+++ app/src/main/java/com/example/ndscalculator/Case1.java
@@ -8,15 +8,18 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Case1 extends AppCompatActivity {
-
     String[] dropDownWoods = new String[]{"Douglas Fir-Larch","Douglas Fir-South",
             "Eastern Softwoods", "Eastern White Pine", "Hem-Fir", "Redwood",
             "Spruce-Pine-Fir", "Spruce-Pine-Fir (South)", "Western Woods"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +36,6 @@ public class Case1 extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         woodSelect.setAdapter(adapter);
-
-
     }
 
     public void setSpinner2(View v)
@@ -45,52 +46,68 @@ public class Case1 extends AppCompatActivity {
         Spinner spinner2 = (Spinner)findViewById(R.id.spinner2);
 
         try {
-            Log.i("log", "retrieved spinners, pulling selection from 1");
-            String selection1 = woodSelect.getSelectedItem().toString(); //this is the problem line
-
-            //bug fixing, looking for reason of failure
-            if(selection1 == null)
-            {
-                Log.i("log","selected item returned null");
-            }
-            else if(selection1 == "")
-            {
-                Log.i("log","selected item returned empty string");
-            }
-            else
-            {
-                Log.i("log", "item is: " + selection1.getClass());
-            }
-
+            //Log.i("log", "retrieved spinners, pulling selection from 1");
+            String selection1 = woodSelect.getSelectedItem().toString();
             Log.i("log", "selection 1 retrieved, should be: " + selection1);
             String[] spinner2Options = getTypesFromWood(selection1);
-            Log.i("log", "\nsuccess retrieving wood array");
+            Log.i("log", "success retrieving wood array");
+            //Log.i("log","first and last value: " + spinner2Options[0] + spinner2Options[spinner2Options.length]);
 
+            //setspinner here
+            Log.i("log", "entering array adapter #2");
+            ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, spinner2Options);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter2);
 
-            Log.i("log", "\nSuccess!!\n");
+            Log.i("log", "success?!");
         }
         catch(Exception e)
         {
             Log.i("log","Failure");
         }
-
-
     }
 
     public String[] getTypesFromWood(String woodType) throws FileNotFoundException {
         //The goal of this monstrosity is to, from the file they selected in the first spinner,
-        //to create a second spinner of the options from within that wood type.
+        //pull the headings from the first position of each line and return that to set as the
+        // 2nd spinner options
+        Log.i("log","we have entered getTypes method");
 
-        //The longest subtype is 14, taking the spinner option and concatenating .csv
-        Log.i("log","making array and adding .csv");
+        //The longest subtype is 14, taking the spinner option and concatenating .txt
         String[] retrieved = new String[14];
 
-        Log.i("log","about to concat");
-        String location = (woodType);
-        location = location.concat(".txt");
+        //lots of logs for bug fixing
+        //location = "\\AndroidDevNDSCalculator\\app\\src\\main\\java\\com\\example\\ndscalculator\\".concat(woodType);
+        String location = woodType.concat(".txt");
         Log.i("log","our file/location should be: " + location);
         Log.i("log","starting file pull attempt");
-        File input = new File(location);
+
+        try {
+            //in case catch fails
+            DataInputStream inputStream = null;
+            //stack overflow?
+            try {
+                inputStream = new DataInputStream(getAssets().open(String.format(location)));
+                Log.i("log","success making input stream");
+            } catch (IOException e) {
+                Log.i("log","input stream failed");
+                e.printStackTrace();
+            }
+            Scanner scan = new Scanner(inputStream);
+            int counter = 0;
+            while (scan.hasNextLine())
+            {
+                String line=scan.nextLine();
+                String[] lineArr = line.split(",");
+
+                retrieved[counter]=lineArr[0];
+                counter++;
+            }
+
+        } catch (Exception e) {
+            Log.i("log","failed to read file");
+        }
 
         //this is going to be a stupid piece of code but java doesn't have easy resize for arrays
         //count actual length of array
@@ -102,27 +119,28 @@ public class Case1 extends AppCompatActivity {
                 actualLength++;
             }
         }
+        Log.i("log","Counted length = " + actualLength + ", about to resize array");
         //resizing/copying the array
-        String[] returnArray = new String[actualLength];
-        for(int k = 0;k<=actualLength;k++)
+        String[] returnArray = new String[actualLength-1];
+        Log.i("log","Made new array, about to copy");
+        for(int k=1;k<actualLength;k++)
         {
-            returnArray[k] = retrieved[k];
+            //Log.i("log","copying value: " + retrieved[k]);
+            //copy all values except the first, the first is the same as the wood type
+            returnArray[k-1] = retrieved[k];
         }
 
+        Log.i("log","Returning the array");
         return returnArray;
-
     }
 
 }
-
-/*        Log.i("log","entering while loop");
-                while (scan.hasNextLine())
-                {
-                String line = scan.nextLine();
-                String[] resultLine = line.split(",");
-                returnArray[i] = resultLine[0];
-                i++;
-                }
-                scan.close();
-                Log.i("returnArray","Return = " + returnArray);
-                return returnArray;*/
+//parse through file
+            //Log.i("log","parsing file");
+            /*while ((line = br.readLine()) != null)
+            {
+                String[] lineArr = line.split(",");
+                //add title of line to return list
+                retrieved[counter]=lineArr[0];
+            }
+        br.close();*/
