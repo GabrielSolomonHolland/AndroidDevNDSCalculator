f6e8603e069754cda738cda5e1c5f87effd9ab2d
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-03-31 10:36:06 -0500

    Fix related error

--- app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
+++ app/src/main/java/com/example/ndscalculator/CalculatorLanding.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+//import android.widget.Button;
 
 public class CalculatorLanding extends AppCompatActivity {
 
