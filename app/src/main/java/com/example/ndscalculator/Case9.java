7df0c3e2902acbc524cd921afa77f416da0db3f8
Jacob Daleske <60079607+jdaleske13@users.noreply.github.com>
2022-04-20 20:24:25 -0500

    Completed all layouts and ads and back buttons

--- app/src/main/java/com/example/ndscalculator/Case9.java
+++ app/src/main/java/com/example/ndscalculator/Case9.java
@@ -2,7 +2,16 @@ package com.example.ndscalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class Case9 extends AppCompatActivity {
 
@@ -10,5 +19,20 @@ public class Case9 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case9);
+        Log.i("log","About to initialize ads");
+        MobileAds.initialize(this,new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete (InitializationStatus initializationStatus){
+            }
+        });
+        AdView madView = findViewById(R.id.c9adBanner);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        madView.loadAd(adRequest);
+        Log.i("log","Ad initialized");
+    }
+    public void returnToCalc(View v)
+    {
+        Intent toCalc = new Intent(this, CalculatorLanding.class);
+        startActivity(toCalc);
     }
 }
--- app/src/main/java/com/example/ndscalculator/CaseCustom.java
+++ app/src/main/java/com/example/ndscalculator/CaseCustom.java
@@ -2,7 +2,9 @@ package com.example.ndscalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CaseCustom extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CaseCustom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_custom);
+    }
+
+    public void returnToCalc(View v)
+    {
+        Intent toCalc = new Intent(this, CalculatorLanding.class);
+        startActivity(toCalc);
     }
 }
